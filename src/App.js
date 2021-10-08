fa3de0ed50f21ee71c4a3ed18a15d485c979c60e
Paramjeet <param@leewayhertz.com>
2021-10-08 04:00:28 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('')
+  const [phonenumber, setPhone] = useState('')
+  // const [loginUsername, setLoginUsername] = useState("");
+  // const [loginPassword, setLoginPassword] = useState("");
+  // const [data, setData] = useState(null);
+  const register = () => {
+    console.log(name, email);
+    axios.post('http://localhost:8000/register',{
+    name, email, phonenumber, password
+    })
+    
+  };
+  const login = () => {
+    axios.post("http://localhost:8000/login",{
+      email, password
+    }).then(res=>{
+      console.log(res.data);
+    })
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Register</h1>
+        <input
+          placeholder="username"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          placeholder="phonenumber"
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        <input
+        placeholder="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+  
+        <input type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={register}>Submit</button>
+      </div>
+
+      <div>
+        <h1>Login</h1>
+        <input
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={login}>Submit</button>
+      </div>
+
+      <div>
+        
+      
+      
+      </div>
     </div>
   );
 }
